b43d57fa7ee5c3187e2750b6866ab7bf48b13099
alyssa-daigle <143747222+alyssa-daigle@users.noreply.github.com>
2024-09-17 19:24:47 -0400

    Add files via upload
    updated image processing to include file type specifications

--- analyzer/cli.py
+++ analyzer/cli.py
@@ -3,6 +3,7 @@ from analyzer.well_detector import WellDetector
 from analyzer.well_analyzer import WellAnalyzer
 from analyzer.visualizer import Visualizer
 from analyzer.hsv_thresholder import HsvThresolder
+from analyzer.config_manager import ConfigManager
 from pathlib import Path
 import os
 import click
@@ -28,9 +29,27 @@ def cli():
     type=click.INT,
     help="The desired width to display the image.",
 )
-def threshold(image, width):
+@click.option(
+    "-c",
+    "--config",
+    "config",
+    type=click.Path(path_type=Path),
+    help="The config to be updated with the chosen HSV range.",
+)
+def threshold(image, width, config):
+    """Helper to determine optimal threshold values for masking items of interest"""
     thresholder = HsvThresolder(image)
-    thresholder.threshold(width)
+    lower_bound, upper_bound = thresholder.threshold(width)
+    new_min_max_threshold = {
+        "well_analyzer": {
+            "hsv_lower_bound": lower_bound,
+            "hsv_upper_bound": upper_bound
+        }
+    }
+    config_manager = ConfigManager(config)
+    config_manager.update(new_min_max_threshold)
+    config_manager.write()
+    click.echo(f"Updated thresholds in {config}")
 
 @cli.command()
 @click.option(
@@ -41,6 +60,19 @@ def threshold(image, width):
     multiple=False,
     help="The image file to open.",
 )
+@click.option('--dp', default=1, help='Inverse ratio of the accumulator resolution to the image resolution.')
+@click.option('--min_dist', default=270, help='Minimum distance between the centers of detected circles.')
+@click.option('--param1', default=45, help='First method-specific parameter for the edge detection.')
+@click.option('--param2', default=20, help='Second method-specific parameter for the center detection.')
+@click.option('--min_radius', default=120, help='Minimum circle radius to detect.')
+@click.option('--max_radius', default=145, help='Maximum circle radius to detect.')
+@click.option(
+    "-c",
+    "--config",
+    "config",
+    type=click.Path(path_type=Path),
+    help="The config to use. Cannot be combined with individual params for configuration.",
+)
 @click.option(
     "-o",
     "--output",
@@ -49,9 +81,40 @@ def threshold(image, width):
     multiple=False,
     help="The name of the directory to output to.",
 )
-def process(images, output):
-    def process_image(image_path, output):
+def process(images, config, output, dp, min_dist, param1, param2, min_radius, max_radius):
+    """Processes images"""
+    
+    hsv_lower_bound = (20, 18, 0)
+    hsv_upper_bound =  (179, 255, 91)
 
+    if config:
+        try:
+            config_manager = ConfigManager(config)
+            config_manager.load()
+            
+            well_detector_config = config_manager.get('well_detector')
+            dp = well_detector_config.get('dp')
+            min_dist = well_detector_config.get('min_dist')
+            param1 = well_detector_config.get('param1')
+            param2 = well_detector_config.get('param2')
+            min_radius = well_detector_config.get('min_radius')
+            max_radius = well_detector_config.get('max_radius')
+
+            well_analyzer_config = config_manager.get('well_analyzer')
+            hsv_lower_bound = tuple(well_analyzer_config.get('hsv_lower_bound'))
+            hsv_upper_bound = tuple(well_analyzer_config.get('hsv_upper_bound'))
+        except:
+            click.echo(f"Failed while process config file {config}")
+    else:
+        # If no config file, use CLI values with defaults
+        dp = dp or 1
+        min_dist = min_dist or 270
+        param1 = param1 or 45
+        param2 = param2 or 20
+        min_adius = min_radius or 120
+        max_radius = max_radius or 145
+
+    def process_image(image_path, output):
         click.echo(f'Processing {image_path}')
         # Step 1: Load and preprocess the image
         processor = ImageProcessor(image_path)
@@ -59,11 +122,11 @@ def process(images, output):
 
         # Step 2: Detect and sort circles (wells)
         detector = WellDetector(blurred_image)
-        detected_circles = detector.detect_wells()
+        detected_circles = detector.detect_wells(dp, min_dist, param1, param2, min_radius, max_radius)
         sorted_circles = detector.sort_circles(detected_circles)
 
         # Step 3: Analyze duckweed for each well
-        analyzer = WellAnalyzer(processor.get_original_image())
+        analyzer = WellAnalyzer(processor.get_original_image(), hsv_lower_bound, hsv_upper_bound)
         visualizer = Visualizer(processor.get_original_image(), sorted_circles)
 
         csv_out = []
@@ -81,7 +144,6 @@ def process(images, output):
 
         # Step 4: Display the final annotated image
         visualizer.draw_circles()
-        #visualizer.show_image('Identified Wells', 1440)
 
         os.makedirs(output, exist_ok=True)
         out_name = os.path.splitext(os.path.basename(image_path))[0]
@@ -93,13 +155,30 @@ def process(images, output):
         out_image = str(output) + "/" + out_name + "_annotated.png"
         visualizer.save_image(out_image)
 
-
     if images.is_dir():
         for image_path in images.iterdir():
-            process_image(image_path, output)
+            if image_path.suffix.lower() in ['.png', '.jpg', '.jpeg']:
+                process_image(image_path, output)
     else:
-        process_image(images, output)
+        if images.suffix.lower() in ['.png', '.jpg', '.jpeg']:
+            process_image(images, output)
 
+@cli.command()
+@click.option(
+    "-f",
+    "--file",
+    "file",
+    type=click.Path(path_type=Path),
+    help="The config file path.",
+)
+def config(file):
+    """Generates a new config file"""
+    if file:
+        config_manager = ConfigManager(file)
+        config_manager.generate()
+    else:
+        config_manager = ConfigManager()
+        config_manager.generate()
 
 # # Run the program
 if __name__ == "__main__":
